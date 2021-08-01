6ab2ee5e1c06689251a2d7d823c14df1de039aa8
Alessandro <asovitorio@gmail.com>
2021-08-01 02:32:30 -0300

    :art: adjust of layout

--- src/public/javascripts/javascript.js
+++ src/public/javascripts/javascript.js
@@ -1,19 +1,61 @@
+// ####### 1 Controle de scroll coloca a sombra no nav ##########
+const header = document.querySelector('#header')
+const navHeigth = header.offsetHeight
+console.log(navHeigth);
+window.addEventListener('scroll', e => {
+    if (window.scrollY >= navHeigth) {
+        header.classList.add('scroll')
+        
+        header.style.transition = "0.4s"
+    } else {
+        header.classList.remove('scroll')
+        
+    }
+})
+// ########## FIM  1 Controle de scroll coloca a sombra no nav ############
 
-
-//###### 1 Controle: abre e fecha o menu ########
-const nav = document.querySelector('#header nav')
+//###### 2 Controle: abre e fecha o menu ########
+const nav = document.querySelector('#header')
 const toogle = document.querySelectorAll('nav .toogle')
-const links =document.querySelectorAll('nav ul a')
+const links = document.querySelectorAll('nav ul a')
 
 for (const element of toogle) {
-    element.addEventListener('click',e => {
-       nav.classList.toggle('show')
+    element.addEventListener('click', e => {
+        nav.classList.toggle('show')
     })
 }
 
 for (const link of links) {
-    link.addEventListener('click', e =>{
+    link.addEventListener('click', e => {
         nav.classList.remove('show')
     })
 }
-// ########## FIM  1 Controle ############
+// ########## FIM  2 Controle ############
+
+// ########## 3 CONTROLE DO SLIDERS ############
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView:1,
+   pagination:{
+      el:'.swiper-pagination' 
+   },
+   mousewheel:true,
+   keyboard:true,
+
+  });
+
+// SCROL REVIEW 
+
+const scrollReveal = ScrollReveal({
+    origin:"top",
+    distance:'30px',
+    duration:700,
+    reset:true
+})
+
+scrollReveal.reveal(`
+#home .image,#home .text,
+#about .image, #about .title-2, #about .text p,
+#service .cards title-2, #service .cards p, #service .cards .card,
+#events .title-2,#events .events,
+#contact .title-2, #contact .ilustract,#contact .text p, #contact .text a, #contact .links
+`,{interval:100})
