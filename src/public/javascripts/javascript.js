5fadb28a752dedd78b47927b02a7553e4c06416e
Alessandro <asovitorio@gmail.com>
2021-08-12 14:19:40 -0300

    ajust

--- src/public/javascripts/javascript.js
+++ src/public/javascripts/javascript.js
@@ -15,7 +15,21 @@ window.addEventListener('scroll', e => {
 // ########## FIM  1 Controle de scroll coloca a sombra no nav ############
 
 //###### 2 Controle: abre e fecha o menu ########
+const nav = document.querySelector('.container-2')
+const toogle = document.querySelectorAll('nav .toogle')
+const links = document.querySelectorAll('nav ul a')
 
+for (const element of toogle) {
+    element.addEventListener('click', e => {
+        nav.classList.toggle('show')
+    })
+}
+
+for (const link of links) {
+    link.addEventListener('click', e => {
+        nav.classList.remove('show')
+    })
+}
 // ########## FIM  2 Controle ############
 
 // ########## 3 CONTROLE DO SLIDERS ############
@@ -39,10 +53,10 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(`
-#home,
+#home .image,#home .text,
 #about .image, #about .title-2, #about .text p,
 #service .cards title-2, #service .cards p, #service .cards .card,
 #events .title-2,#events .events,
 #contact .title-2, #contact .ilustract,#contact .text p, #contact .text a, #contact .links,
-#footer,.container-2 .grid
+#footer,.container .grid
 `,{interval:100})
